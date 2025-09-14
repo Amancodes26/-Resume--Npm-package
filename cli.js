bf737ac6ed0e110e42da1ec48657a42c6cd9f793
Amancodes26 <amansngh8586@gmail.com>
2025-09-14 12:57:20 +0530

    My Resume as an NPM Package

---
+++ cli.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+
+console.clear();
+
+console.log(chalk.cyan.bold("\n👋 Hi, I am Aman Singh\n"));
+
+console.log(chalk.green("📧 Email:"), "amansngh8586@gmail.com");
+console.log(chalk.green("🔗 LinkedIn:"), "linkedin.com/in/aman-singh-2026s");
+console.log(chalk.green("🐙 GitHub:"), "github.com/Amancodes26");
+console.log(chalk.green("🌐 Portfolio:"), "https://devamanai.vercel.app/\n");
+
+console.log(chalk.yellow.bold("Summary:"));
+console.log("Developer focused on backend systems, API design, and full-stack collaboration.\n");
+
+console.log(chalk.yellow.bold("Education:"));
+console.log("- B.Tech in CSE, G.B. Pant Govt. College of Engineering (2023–2026)");
+console.log("- Diploma in Civil Engineering, Pusa Institute of Technology (90%, 2020–2023)\n");
+
+console.log(chalk.yellow.bold("Experience:"));
+console.log(chalk.magenta("Software Developer Engineer Intern – DSEU (Jun–Aug 2025)"));
+console.log("  • Built and deployed a Grievance Management Portal with Next.js, Node.js, SQL.\n");
+console.log(chalk.magenta("Software Developer Engineer Intern – Moreton Bay (Jun–Jul 2024)"));
+console.log("  • Improved frontend UX and streamlined backend form handling.\n");
+
+console.log(chalk.yellow.bold("Achievements:"));
+console.log("🏆 Top 4 – IIIT-D Infornix Hackathon (SchemeSeva)");
+console.log("🏆 Top 3 – DSEU Hackathon (Note-management website)\n");
+
+console.log(chalk.magenta("FrameFusionAI"));
+console.log("  • AI-powered image generation app (Next.js, Pollination AI, Razorpay).");
+console.log("  🔗 GitHub: https://github.com/Amancodes26/FrameFusion");
+console.log("  🌐 Live:   https://framefusionai.vercel.app/\n");
+
+console.log(chalk.magenta("Devamansingh – NPM Package"));
+console.log("  • Published personal branding package on NPM.");
+console.log("  🔗 GitHub: https://github.com/Amancodes26/npm-package-devamansingh");
+console.log("  🌐 Live:   https://www.npmjs.com/package/devamansingh\n");
+
+console.log(chalk.yellow.bold("Open Source Contributions:"));
+console.log("  • CampusBurn – Revamped login/signup UI with Next.js, Tailwind, TS.");
+console.log("  🔗 PullRequest: https://github.com/poswalsameer/CampusBurn/pull/51 ");
+console.log("  🌐 Issuse:  https://github.com/poswalsameer/CampusBurn/issues/49\n");
+
+
+
+console.log(chalk.yellow.bold("Technical Skills:"));
+console.log("• Languages: JavaScript, Python");
+console.log("• Backend: API Design, JWT, MongoDB, Prisma ORM, Express, Docker");
+console.log("• Frameworks: MERN, Next.js, React.js, Node.js");
+console.log("• Databases: MongoDB, MySQL");
+console.log("• Tools: Git, GitHub, Postman, Insomnia, Linux\n");
+
+console.log(chalk.cyan.bold("✨ Thanks for viewing my resume via NPM! ✨\n"));
